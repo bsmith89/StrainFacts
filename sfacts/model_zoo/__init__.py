378739e3cafc51de998ad8330ddac440a14300b8
Byron Smith <me@byronjsmith.com>
2021-10-06 12:53:03 -0700

    Add a highly reduced model.

--- sfacts/model_zoo/__init__.py
+++ sfacts/model_zoo/__init__.py
@@ -2,10 +2,12 @@ from sfacts.model_zoo import (
     base,
     new_pi_regularization,
     with_missing,
+    simple,
 )
 
 NAMED_STRUCTURES = dict(
     default=base.model,
     new_regularization=new_pi_regularization.model,
     missing=with_missing.model,
+    simple=simple.model,
 )
