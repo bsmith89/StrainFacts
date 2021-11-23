0116daf5bcfc09d84906fbf5e7cd77746c2ee33f
Byron Smith <me@byronjsmith.com>
2021-11-23 10:19:20 -0800

    Fix second-round nposition parameter usage

--- sfacts/workflow.py
+++ sfacts/workflow.py
@@ -118,14 +118,17 @@ def fit_subsampled_metagenotypes_then_collapse_and_iteratively_refit_genotypes(
         estimation_kwargs = {}
     if stage2_hyperparameters is None:
         stage2_hyperparameters = {}
-    if npositionB is None:
-        npositionB = nposition
 
     _info(f"START: Fitting data with shape {metagenotypes.sizes}.")
 
     _info(f"Subsampling {nposition} positions.")
     sf.pyro_util.set_random_seed(seed, warn=(not quiet))
     nposition = min(nposition, metagenotypes.sizes["position"])
+    if npositionB is None:
+        npositionB = nposition
+    else:
+        npositionB = min(npositionB, metagenotypes.sizes["position"])
+
     metagenotypes_full = metagenotypes
     metagenotypes = metagenotypes.random_sample(position=nposition)
 
