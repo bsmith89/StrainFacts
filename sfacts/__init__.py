6344ff4566b2523a53fa1a78f3014aea47fbdddc
Byron Smith <me@byronjsmith.com>
2021-11-29 10:56:45 -0800

    Easier ergonomics by exposing commonly used functions in top-level package namespace.

--- sfacts/__init__.py
+++ sfacts/__init__.py
@@ -12,3 +12,30 @@ from sfacts import (
     data,
     app,
 )
+from sfacts.model import ParameterizedModel
+from sfacts.plot import (
+    plot_genotype,
+    plot_community,
+    plot_metagenotype,
+    plot_depth,
+    plot_loss_history,
+    plot_metagenotype_frequency_spectrum,
+)
+from sfacts.data import Metagenotypes, World
+from sfacts.evaluation import (
+    match_genotypes,
+    discretized_genotype_error,
+    genotype_error,
+    discretized_genotype_error,
+    weighted_genotype_error,
+    discretized_weighted_genotype_error,
+    braycurtis_error,
+    metagenotype_error,
+    unifrac_error,
+    unifrac_error2,
+)
+from sfacts.workflow import (
+    simulate_world,
+    fit_metagenotypes_simple,
+    fit_subsampled_metagenotypes_then_collapse_and_iteratively_refit_genotypes,
+)
