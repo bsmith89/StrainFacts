14edf2708a26e523a605fac135e6595fd2c2760a
Byron Smith <me@byronjsmith.com>
2022-02-01 12:09:25 -0800

    Add some more obvious genotype dissimilarities useful for a broad audience.

--- sfacts/math.py
+++ sfacts/math.py
@@ -37,8 +37,45 @@ def genotype_dissimilarity(x, y, q=2):
     return np.where(np.isnan(wmean_dist), dist.mean(), wmean_dist)
 
 
+def discrete_genotype_dissimilarity(x, y, q=2):
+    "Dissimilarity between 1D genotypes, accounting for fuzzyness."
+    x = genotype_binary_to_sign(x)
+    y = genotype_binary_to_sign(y)
+    x_sign, y_sign = np.sign(x), np.sign(y)
+
+    dist = np.abs((x_sign - y_sign) / 2)
+    weight = np.abs(x * y)
+    wmean_dist = ((weight * dist).sum()) / ((weight.sum()))
+    # Why not finish up by powering it by (1 / q)?
+    # I don't do this part because it loses the city-block distance
+    # interpretation when x and y are both discrete (i.e. one of {0, 1}).
+
+    # While the basic function is undefined where weight.sum() == 0
+    # (and this is only true when one of x or y is always exactly 0.5 at every
+    # index),
+    # the limit approaches the same value from both directions.
+    # We therefore redefine the dissimilarity as a piecewise function,
+    # but one that is nonetheless everywhere smooth and defined.
+    return np.where(np.isnan(wmean_dist), dist.mean(), wmean_dist)
+
+
+def genotype_masked_hamming_distance(x, y):
+    x, y = np.asarray(x), np.asarray(y)
+    mask = ~(np.isnan(x) | np.isnan(y))
+    diff = np.abs(x - y)
+    return diff[mask].sum() / np.ones_like(diff)[mask].sum()
+
+
 def genotype_cdist(xx, yy, q=2):
     return cdist(xx, yy, genotype_dissimilarity, q=q)
+
+
+def discrete_genotype_cdist(xx, yy):
+    return cdist(xx, yy, discrete_genotype_dissimilarity)
+
+
+def genotype_masked_hamming_cdist(xx, yy):
+    return cdist(xx, yy, genotype_masked_hamming_distance)
 
 
 def genotype_pdist(xx, quiet=True, q=2):
