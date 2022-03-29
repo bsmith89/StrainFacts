7721df157da07ef5bf5d8b4cb0f0f56407adc671
Byron Smith <me@byronjsmith.com>
2022-03-29 10:41:53 -0700

    Add a scratch model for development.

--- sfacts/model_zoo/__init__.py
+++ sfacts/model_zoo/__init__.py
@@ -1,6 +1,7 @@
 from sfacts.model_zoo import (
     simplest_simulation,
     ssdd3_with_error,
+    new_model_1
 )
 
 NAMED_STRUCTURES = dict(
@@ -8,4 +9,5 @@ NAMED_STRUCTURES = dict(
     default_simulation=simplest_simulation.model,
     ssdd3_with_error=ssdd3_with_error.model,
     simplest_simulation=simplest_simulation.model,
+    new_model_1=new_model_1.model,
 )
---
+++ sfacts/model_zoo/new_model_1.py
@@ -0,0 +1,103 @@
+import sfacts as sf
+from sfacts.model_zoo.components import (
+    _mapping_subset,
+    SHARED_DESCRIPTIONS,
+    SHARED_DIMS,
+    ShiftedScaledDirichlet,
+)
+import torch
+import pyro
+import pyro.distributions as dist
+
+
+@sf.model.structure(
+    dims=SHARED_DIMS,
+    description=_mapping_subset(
+        SHARED_DESCRIPTIONS,
+        [
+            "rho",
+            "p",
+            "mu",
+            "m",
+            "y",
+            "genotypes",
+            "communities",
+            "metagenotypes",
+            "mu",
+        ],
+    ),
+    default_hyperparameters=dict(
+        gamma_hyper=1e-10,
+        rho_hyper=0.5,
+        pi_hyper=0.3,
+        mu_hyper_mean=1.0,
+        mu_hyper_scale=1.0,
+        m_hyper_concentration=1.0,
+        epsilon=0.01,
+    ),
+)
+def model(
+    n,
+    g,
+    s,
+    a,
+    gamma_hyper,
+    rho_hyper,
+    pi_hyper,
+    mu_hyper_mean,
+    mu_hyper_scale,
+    epsilon,
+    m_hyper_concentration,
+    _unit,
+):
+    with pyro.plate("position", g, dim=-1):
+        with pyro.plate("strain", s, dim=-2):
+            gamma_logit = pyro.sample(
+                "gamma_logit",
+                dist.Laplace(loc=0, scale=1 / gamma_hyper),
+            )
+    gamma = torch.special.expit(gamma_logit)
+    pyro.deterministic("genotypes", gamma)
+
+    # Meta-community composition
+    rho = pyro.sample(
+        "rho",
+        ShiftedScaledDirichlet(_unit.repeat(s), _unit.repeat(s) / s, 1 / rho_hyper),
+    )
+    pyro.deterministic("metacommunity", rho)
+
+    with pyro.plate("sample", n, dim=-1):
+        # Community composition
+        pi = pyro.sample(
+            "pi", ShiftedScaledDirichlet(_unit.repeat(s), rho, 1 / pi_hyper)
+        )
+        mu = pyro.sample(
+            "mu", dist.LogNormal(loc=torch.log(mu_hyper_mean), scale=mu_hyper_scale)
+        )
+    pyro.deterministic("communities", pi)
+
+    m = pyro.sample(
+        "m",
+        dist.GammaPoisson(
+            rate=m_hyper_concentration / mu.reshape((-1, 1)),
+            concentration=m_hyper_concentration,
+        )
+        .expand([n, g])
+        .to_event(),
+    )
+
+    # Expected fractions of each allele at each position
+    p_noerr = pyro.deterministic("p_noerr", pi @ gamma)
+    p = pyro.deterministic(
+        "p", (1 - epsilon / 2) * (p_noerr) + (epsilon / 2) * (1 - p_noerr)
+    )
+
+    # Observation
+    y = pyro.sample(
+        "y",
+        dist.Binomial(
+            probs=p,
+            total_count=m,
+        ).to_event(),
+    )
+    pyro.deterministic("metagenotypes", torch.stack([y, m - y], dim=-1))
