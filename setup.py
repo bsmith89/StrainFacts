e7c4ca8aba97d7a979ee44579b1248dbfb18ef1c
Byron Smith <me@byronjsmith.com>
2022-04-12 14:37:19 -1000

    Expand install_requires

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
 from setuptools import setup
 
 
-setup(name='StrainFacts',
-      version='0.1',
-      summary='TODO',
-      url='http://github.com/bsmith89/StrainFacts',
-      author='Byron J. Smith',
-      author_email='me@byronjsmith.com',
-      packages=['sfacts'],
-      install_requires=[],
-      dependency_links=[],
-      entry_points={
-        'console_scripts': [
-          'sfacts = sfacts.__main__:main'
-        ]
-      },
-      zip_safe=False
+setup(
+    name="StrainFacts",
+    version="0.1",
+    summary="TODO",
+    url="http://github.com/bsmith89/StrainFacts",
+    author="Byron J. Smith",
+    author_email="me@byronjsmith.com",
+    packages=["sfacts"],
+    install_requires=[
+        "pytorch",
+        "pyro-ppl",
+        "xarray",
+        "netcdf4",
+        "pandas",
+        "matplotlib",
+        "numpy",
+        "scipy",
+        "tqdm",
+        "scikit-learn",
+    ],
+    dependency_links=[],
+    entry_points={"console_scripts": ["sfacts = sfacts.__main__:main"]},
+    zip_safe=False,
 )
