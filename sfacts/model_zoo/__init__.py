a0cba35abaf347fb014eb28be588cb7c0a8f8ce6
Byron Smith <me@byronjsmith.com>
2022-04-22 13:28:36 -0700

    Rename some model modules for simplicity.

--- sfacts/model_zoo/__init__.py
+++ sfacts/model_zoo/__init__.py
@@ -1,15 +1,14 @@
 from sfacts.model_zoo import (
-    simplest_simulation,
-    ssdd3_with_error,
+    simulation0,
+    model1,
     model2,
 )
 
 NAMED_STRUCTURES = dict(
     default=model2.model,
-    model1=ssdd3_with_error.model,
-    default_simulation=simplest_simulation.model,
-    ssdd3_with_error=ssdd3_with_error.model,
-    simplest_simulation=simplest_simulation.model,
-    simulation0=simplest_simulation.model,
+    model1=model1.model,
+    default_simulation=simulation0.model,
+    ssdd3_with_error=model1.model,
+    simulation0=simulation0.model,
     model2=model2.model,
 )
