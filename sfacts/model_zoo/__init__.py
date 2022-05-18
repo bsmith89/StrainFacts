e755c83b4914e0d5f77287684f2b8e43c484129d
Byron Smith <me@byronjsmith.com>
2022-05-17 21:19:16 -0700

    Make model5 the default model.

--- sfacts/model_zoo/__init__.py
+++ sfacts/model_zoo/__init__.py
@@ -8,7 +8,7 @@ from sfacts.model_zoo import (
 )
 
 NAMED_STRUCTURES = dict(
-    default=model2.model,
+    default=model5.model,
     model1=model1.model,
     default_simulation=simulation0.model,
     ssdd3_with_error=model1.model,
